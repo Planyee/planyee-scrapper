5a1f4ffd3a481d7f87a0c827c85b4a1f8fcbda62
sheepseung <qkffldlfdjsk12@naver.com>
2023-10-27 20:31:36 +0900

    setting파일 제외

--- app/settings.py
+++
@@ -1,7 +0,0 @@
-DATABASES = {
-    'user': 'planyee_admin',
-    'passwd': 'gdsc-planyee!',
-    'host': 'freetier-database.c5rdslabxigz.ap-northeast-2.rds.amazonaws.com',
-    'db': 'planyee_db'
-}
-
